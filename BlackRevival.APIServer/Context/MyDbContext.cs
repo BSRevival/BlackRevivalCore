80b1977ce7b4db7200c5482aa0edf7c2ebbed65b
BSRevival <129611452+BSRevival@users.noreply.github.com>
2023-09-10 19:45:29 -0700

    update database inventory

---
+++ BlackRevival.APIServer/Context/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using BlackRevival.APIServer.Migrations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackRevival.APIServer.Context;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;database=blacksurvival;uid=root;pwd=johnsond", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.2.0-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
