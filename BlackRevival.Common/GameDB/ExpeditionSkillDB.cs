d96286ed80fc21446a32904a8218ae97b01fa132
Julio Takagui <takaguij@gmail.com>
2023-09-18 16:03:54 -0300

    Add ExpeditionSkillDB

---
+++ BlackRevival.Common/GameDB/ExpeditionSkillDB.cs
@@ -0,0 +1,40 @@
+﻿using BlackRevival.Common.Model;
+using BlackRevival.Common.Enums;
+using BlackRevival.Common.GameDB.Expedition;
+using Serilog;
+
+namespace BlackRevival.Common.GameDB;
+
+public class ExpeditionSkillDB
+{
+    public ExpeditionSkillDB()
+    {
+        expeditionSkill = new List<ExpeditionSkillData>();
+        Instance = this;
+    }
+
+    public ExpeditionSkillDB(ExpeditionSkillDB.Model model)
+    {
+        expeditionSkill = model.expeditionSkill;
+        Instance = this;
+    }
+
+    public ExpeditionSkillData Find(ExpeditionSkillId type)
+    {
+        return this.Find((int)type);
+    }
+
+    public ExpeditionSkillData Find(int skillId)
+    {
+        return this.expeditionSkill.Find((ExpeditionSkillData skill) => skill.expeditionSkillId == skillId);
+    }
+
+    public List<ExpeditionSkillData> expeditionSkill { get; set; }
+    public static ExpeditionSkillDB Instance { get; private set; }
+
+    public class Model
+    {
+        public List<ExpeditionSkillData> expeditionSkill { get; set; }
+    }
+
+}
